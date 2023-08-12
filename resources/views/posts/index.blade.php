9d4719033227245c55f0485005c9b8fb0e1bff76
KazukiFootball14 <kazuki.football14@gmail.com>
2023-08-12 10:00:23 +0900

    Add 初期設定&基礎実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+        
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
@@ -18,5 +18,6 @@
                 </div>
             @endforeach
         </div>
+        <div class='paginate'>{{ $posts->links()}}</div>
     </body>
 </html>
